2d121d9f3b2eb8d4749d2985dcfb5f40368f7a38
aryantiwari14m-pixel <aryantiwari14m@gmail.com>
2026-05-02 12:57:39 +0530

    Delete Q10.sql
    Removed unnecessary file

--- Q10.sql
+++
@@ -1,8 +0,0 @@
-select p.division,s.product_code,p.product,
-sum(sold_quantity) as total_sales_qty
-,dense_rank() over(order by sum(s.sold_quantity) desc) as Rank_order 
- from gdb023.fact_sales_monthly s join dim_product p  
- on s.product_code=p.product_code 
- group by p.division,s.product_code,p.product
- order by total_sales_qty desc
- limit 3;
