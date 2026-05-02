f77b05b1ddbb1df066682582c165aa5057c95286
aryantiwari14m-pixel <aryantiwari14m@gmail.com>
2026-05-02 12:58:12 +0530

    Delete Q3.sql
    Removed unnecessary file

--- Q3.sql
+++
@@ -1,5 +0,0 @@
-SELECT count(distinct s.product_code) as product_count,segment 
-FROM dim_product p join fact_sales_monthly s on p.product_code
-=s.product_code 
- group by segment 
- order by product_count desc
