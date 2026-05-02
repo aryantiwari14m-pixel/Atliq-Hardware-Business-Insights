38aeb1fdd4ca1494e1dc44accf352a523692527c
aryantiwari14m-pixel <aryantiwari14m@gmail.com>
2026-05-02 12:59:43 +0530

    Delete Q7.sql
    Removed unnecessary file

--- Q7.sql
+++
@@ -1,5 +0,0 @@
-select monthname(date) as Month
-,year(date_add(date,interval 4 month)) as Fiscal_year,
-round((sold_quantity*gross_sales),2)
-as Gross_sales_amt from gross_sales s join dim_customer d
-on s.customer_code=d.customer_code where d.customer = "Atliq Exclusive"
