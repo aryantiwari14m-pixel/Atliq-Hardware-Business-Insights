9e19a8ae6596317a8d62560e287231770ac2be8f
aryantiwari14m-pixel <aryantiwari14m@gmail.com>
2026-05-02 13:00:04 +0530

    Delete Q9.sql
    Removed unnecessary file

--- Q9.sql
+++
@@ -1,9 +0,0 @@
-with cte1 as (select c.channel,round(sum(gross_sales)/1000000,2) 
-as Gross_mln from gdb023.gross_sales s join dim_customer c 
-on s.customer_code=c.customer_code
-where fiscal_year =2021
-group by c.channel
-order by Gross_mln desc )
-
-select *,round(Gross_mln*100/sum(Gross_mln) over(),2)  
-as Percentage from cte1;
